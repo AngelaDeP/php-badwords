4f9866a3a99542a5a9334e6685d215ace35afc91
Angela De Pasquale <angeladepasquale_@libero.it>
2022-03-09 15:13:37 +0100

    Aggiunto il primo paragrafo

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$paragrafo = 'Ecco il mio primo paragrafo tramite PHP';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PARAGRAFO</h1>
+    <p><?php echo $paragrafo ?></p>
+</body>
+</html>
